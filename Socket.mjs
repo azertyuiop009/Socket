e2ae44da55cf930916c4baf2cb057860824e11ec
adazdazdazefgzefzefdsfsefdfsfxfxfsxfsfesfsfdfesdfsefs <77641591+azertyuiop009@users.noreply.github.com>
2021-10-31 18:29:34 +0100

    Update Socket.mjs

--- Socket.mjs
+++ Socket.mjs
@@ -1,7 +1,7 @@
-import WebSocket from 'ws';
-import http from "http";
-import express from "express";
-import { Server } from 'socket.io'
+const WebSocket = require('ws');
+const http = require("http");
+const express = require("express");
+const { Server } = require('socket.io')
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server)
